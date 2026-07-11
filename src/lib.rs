a9fcb1d3eb7febb950c40ed86337f88d31b3eadb
Inam Ul Haq <inam123451@gmail.com>
2026-07-11 12:26:42 +0530

    feat: Add Layer constructor helper

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,24 @@ pub mod error;
 
 mod proto {
     tonic::include_proto!("fvs2d.v1");
+
+    impl Layer {
+        pub fn new(repository: &Repository, commit: Option<&Commit>) -> Self {
+            Layer {
+                repository_path: repository.repository_path.clone(),
+                revision: commit.map(|commit| CommitSelector {
+                    selector: Some(commit_selector::Selector::StateIdOrPrefix(
+                        commit.state_id.clone(),
+                    )),
+                }),
+            }
+        }
+    }
 }
 
 pub use client::Fvs2dClient;
 pub use prost_types::Timestamp;
-pub use proto::{
-    Commit, CommitSelector, Layer, Mount, Repository, RestoreResponse, UnmountMode,
-    commit_selector::Selector,
-};
+pub use proto::{Commit, Layer, Mount, Repository, RestoreResponse, UnmountMode};
 
 #[cfg(test)]
 mod tests {}
