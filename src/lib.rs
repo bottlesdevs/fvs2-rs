01db99bdbab26a26b5465bdfa990abc52aa8c11f
Inam Ul Haq <inam123451@gmail.com>
2026-07-14 22:46:57 +0530

    Move Layer::new out of proto module

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,24 @@ pub mod error;
 
 mod proto {
     tonic::include_proto!("fvs2d.v1");
-
-    impl Layer {
-        pub fn new(repository: &Repository, commit: Option<&Commit>) -> Self {
-            Layer {
-                repository_path: repository.repository_path.clone(),
-                revision: commit.map(|commit| CommitSelector {
-                    selector: Some(commit_selector::Selector::StateIdOrPrefix(
-                        commit.state_id.clone(),
-                    )),
-                }),
-            }
-        }
-    }
 }
 
 pub use client::Fvs2dClient;
 pub use prost_types::Timestamp;
 pub use proto::{Commit, Layer, Mount, Repository, RestoreResponse, UnmountMode};
+
+impl Layer {
+    pub fn new(repository: &Repository, commit: Option<&Commit>) -> Self {
+        Layer {
+            repository_path: repository.repository_path.clone(),
+            revision: commit.map(|commit| crate::proto::CommitSelector {
+                selector: Some(crate::proto::commit_selector::Selector::StateIdOrPrefix(
+                    commit.state_id.clone(),
+                )),
+            }),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {}
